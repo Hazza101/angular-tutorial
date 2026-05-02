633924f88da7a9e7915885f018c5338e33b5e050
Harri <harri.bain@gmail.com>
2026-05-02 19:27:57 +0100

    Add Home component files and folder

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,21 @@
-import {Component} from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "home",
   imports: [],
-  template: ` <h1>Default</h1> `,
-  styleUrls: ['./app.css'],
+  template: ` <h1>home</h1> `,
+  styleUrls: [],
+})
+export class Home {}
+
+@Component({
+  selector: "app-root",
+  imports: [Home],
+  template: ` <ul>
+    <li><home /></li>
+  </ul>`,
+  styleUrls: ["./app.css"],
 })
 export class App {
-  title = 'default';
+  title = "default";
 }
